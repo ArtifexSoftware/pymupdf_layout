1774a4409e7659a5dd99faebcbdd94f9887d3e4f
Julian Smith <julian.smith@artifex.com>
2026-09-22 12:11:53 +0100

    setup.py: fix to depend on pymupdf_core, not pymupdf.

--- setup.py
+++ setup.py
@@ -252,7 +252,7 @@ p = pipcl.Package(
         'pymupdf-layout',
         g_version,
         requires_dist = [
-                f'PyMuPDF=={g_pymupdf_version}' if g_pymupdf_version else None,
+                f'pymupdf_core=={g_pymupdf_version}' if g_pymupdf_version else None,
                 'pyyaml',
                 'numpy',
                 'onnxruntime',
